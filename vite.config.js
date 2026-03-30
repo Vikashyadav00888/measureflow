3837acea1ce601609d2a601d4dabb37d7f39c2ff
Vikash Yadav <vikashyadav00888@gmail.com>
2026-03-30 10:03:36 +0530

    Remove leftover game build entry

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        game: resolve(__dirname, "game.html"),
       },
     },
   },
